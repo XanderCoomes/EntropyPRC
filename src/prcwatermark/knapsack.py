43cb134995ad075a63ca1b80281f10abb5531306
Xander Coomes <xander.coomes@gmail.com>
2025-12-13 11:43:11 -0800

    knapsack

--- src/prcwatermark/knapsack.py
+++ src/prcwatermark/knapsack.py
@@ -1,8 +1,19 @@
 
+import numpy as np
 
 
+def knapsack(weights, T, increment): 
+    num_t_vals = T * (1 / increment) + 1
+    dp = [[0] * int(num_t_vals)] * len(weights)
+    
 
-def
+
+    print(num_t_vals)
+
+
+    return -42
+    
+    
 
 
 
